1eba527c7d30d05e229b046d23661176a324e438
Romario Lopez <romariolopezc@gmail.com>
2019-12-22 18:03:13 -0600

    Add documentation for HmacSha1.Key

--- src/HmacSha1/Key.elm
+++ src/HmacSha1/Key.elm
@@ -1,4 +1,11 @@
-module HmacSha1.Key exposing (Key, fromBytes, fromString)
+module HmacSha1.Key exposing (Key, fromString, fromBytes)
+
+{-| Build an HMAC Key, either from a String or Bytes. Required to build an
+HmacSha1.Digest
+
+@docs Key, fromString, fromBytes
+
+-}
 
 import Bytes exposing (Bytes)
 import Bytes.Encode as Encode
@@ -6,15 +13,21 @@ import Internals as I
 import SHA1
 
 
+{-| Opaque type representing a Key
+-}
 type alias Key =
     I.Key
 
 
+{-| Builds a Key from a String
+-}
 fromString : String -> Key
 fromString =
     fromBytes << Encode.encode << Encode.string
 
 
+{-| Builds a Key from Bytes
+-}
 fromBytes : Bytes -> Key
 fromBytes bytes =
     let
